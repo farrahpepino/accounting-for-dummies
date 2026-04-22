128e543243866c200d732f2edcc19c419f7e109b
farrahpepino <idcfarrah@gmail.com>
2026-04-22 17:05:21 -0400

    Home

--- client/src/Components/Main/Home/Home.tsx
+++ client/src/Components/Main/Home/Home.tsx
@@ -1,24 +1,46 @@
 // import React from 'react'
 import Sidebar from "../../Shared/Navigation/Sidebar/Sidebar"
 import './Home.css'
+import Logo from "../../Shared/Logo/Logo"
 
 const Home = () => {
   return (
     <div className="Home">
+        <Logo />
         <Sidebar />
+
         <div className="container">
-            <div className="balances">
-            
+            <div className="balances border">
+              <div className="bold lg">Balances</div>
+              <br />
+              <div>
+                <div  className="span">
+                  <div className="bold">Savings</div>
+                  <div><span className="sub">$</span> 10,329.00</div>
+                </div>
+              </div>
             </div>
 
-            <div className="graphs">
+            <br />
+            <br />
 
+            <div className="tracker">
+              <div className="bold lg"> 
+                Track where your money goes
+              </div>
+              {/* <div>
+                <div></div>
+              </div> */}
             </div>
 
-            <button className="transparent underline">
+            <br />
+            <br />
+
+            <button className="transparent underline btn">
                 See past monthly statements
             </button>
         </div>
+
     </div>
   )
 }
