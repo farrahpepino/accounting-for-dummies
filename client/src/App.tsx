3bc6f41619e0274ab312687bb14f6297e54fe21b
farrahpepino <idcfarrah@gmail.com>
2026-04-22 13:30:45 -0400

    client set up

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Sidebar from './Components/Shared/Navigation/Sidebar'
+
+function App() {
+
+  return (
+   <div>
+    <Sidebar/>
+   </div>
+  )
+}
+
+export default App
