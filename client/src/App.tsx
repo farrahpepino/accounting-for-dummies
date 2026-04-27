ba4ee72b04f967b1d6cf36cdf9cb1edaae05949d
farrahpepino <idcfarrah@gmail.com>
2026-04-27 13:35:11 -0400

    protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
-import './App.css'
-import Login from './Pages/Auth/Login/Login'
+import './App.css';
+import ProtectedRoutes from './Utils/ProtectedRoutes';
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Main/Home/Home';
+import Accounts from './Pages/Main/Accounts/Accounts';
+import Transactions from './Pages/Main/Transactions/Transactions';
+import Entry from './Pages/Main/Entry/Entry';
+import Login from './Pages/Auth/Login/Login';
 
 function App() {
 
   return (
-   <div>
-    <Login />
-   </div>
+   <Routes>
+    <Route path="/login"        element={<Login />} />
+
+    <Route element={<ProtectedRoutes />}>
+      <Route path="/"        element={<Home />} />
+      <Route path="/accounts"        element={<Accounts />} />
+      <Route path="/transactions"        element={<Transactions />} />
+      <Route path="/entry"        element={<Entry />} />
+    </Route>
+    
+   </Routes>
   )
 }
 
