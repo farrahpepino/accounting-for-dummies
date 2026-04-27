7869084c3e13c103ebeb01f343905e61e60b8b7e
farrahpepino <idcfarrah@gmail.com>
2026-04-27 13:40:35 -0400

    added more routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,6 +8,13 @@ import Transactions from './Pages/Main/Transactions/Transactions';
 import Entry from './Pages/Main/Entry/Entry';
 import Login from './Pages/Auth/Login/Login';
 
+
+import Account from './Pages/Main/AccountAdd/Account';
+import AccountEdit from './Pages/Main/AccountEdit/AccountEdit';
+import TransactionEdit from './Pages/Main/TransactionEdit/TransactionEdit';
+import Settings from './Pages/Main/Settings/Settings';
+import Statements from './Pages/Main/Statements/Statements';
+
 function App() {
 
   return (
@@ -15,12 +22,19 @@ function App() {
     <Route path="/login"        element={<Login />} />
 
     <Route element={<ProtectedRoutes />}>
+      
       <Route path="/"        element={<Home />} />
       <Route path="/accounts"        element={<Accounts />} />
       <Route path="/transactions"        element={<Transactions />} />
       <Route path="/entry"        element={<Entry />} />
+
+      <Route path='/add-account' element={<Account />}/>
+      <Route path='/edit-account' element={<AccountEdit />} />
+      <Route path='/edit-transaction' element={<TransactionEdit />}/>
+      <Route path='/settings' element={<Settings />}/>
+      <Route path='/statements' element={<Statements />}/>
+
     </Route>
-    
    </Routes>
   )
 }
