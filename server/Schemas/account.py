f2829c213cf41511d466ffe0ff37f3ae867cbb61
farrahpepino <idcfarrah@gmail.com>
2026-04-29 16:32:29 -0400

    requirements

---
+++ server/Schemas/account.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, String, Integer, DateTime
+from datetime import datetime
+from database import Base
+import uuid
+
+
+class Account(Base):
+    __tablename__ = "accounts"
+    
+    id = Column(String(36), primary_key=True, index=True, unique=True, default=lambda:str(uuid.uuid4())) 
+    type = Column(String(10), index=True)
+    bank = Column(String(255), index=True)
+    balance = Column(Integer)
+    date = Column(DateTime, default=datetime.now)
+    last_digits = Column(Integer)
+    
+    
+    
---
+++ server/Schemas/transaction.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer, Date
+from database import Base
+import uuid
+
+
+class Transaction(Base):
+    __tablename__ = "accounts"
+    
+    id = Column(String(36), primary_key=True, index=True, unique=True, default=lambda:str(uuid.uuid4())) 
+    type = Column(String(10), index=True)
+    from_bank = Column(String(36))
+    to_bank = Column(String(36))
+    category = Column(String(10))
+    amount = Column(Integer)
+    date = Column(Date)    
+    
+    
