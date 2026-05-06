cd448f72599451ed3def31597ae8038c406a354e
farrahpepino <idcfarrah@gmail.com>
2026-05-06 16:11:55 -0400

    on delete cascades

--- server/Schemas/account.py
+++ server/Schemas/account.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
 from datetime import datetime
 from database import Base
 import uuid
@@ -8,7 +8,12 @@ class Account(Base):
     __tablename__ = "accounts"
     
     id = Column(String(36), primary_key=True, index=True, unique=True, default=lambda:str(uuid.uuid4())) 
-    user_id = Column(String(36), index=True)
+    user_id = Column(
+        String(36),
+        ForeignKey("users.id", ondelete="CASCADE"),
+        index=True,
+        nullable=False
+    )
     type = Column(String(10), index=True)
     bank = Column(String(255), index=True)
     balance = Column(Integer)
--- server/Schemas/transaction.py
+++ server/Schemas/transaction.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, Date
+from sqlalchemy import Column, String, Integer, Date, ForeignKey
 from database import Base
 import uuid
 
@@ -7,7 +7,12 @@ class Transaction(Base):
     __tablename__ = "transactions"
     
     id = Column(String(36), primary_key=True, index=True, unique=True, default=lambda:str(uuid.uuid4())) 
-    user_id = Column(String(36), index=True)
+    user_id = Column(
+        String(36),
+        ForeignKey("users.id", ondelete="CASCADE"),
+        index=True,
+        nullable=False
+    )
     type = Column(String(10), index=True)
     from_bank = Column(String(36))
     to_bank = Column(String(36))
