b30c1a3eebe522bdb1958e40bddff9a19933178e
farrahpepino <idcfarrah@gmail.com>
2026-05-06 21:55:24 -0400

    c

--- client/src/Pages/Main/Transactions/Transactions.tsx
+++ client/src/Pages/Main/Transactions/Transactions.tsx
@@ -14,7 +14,7 @@ const Transactions = () => {
             id: 1,
             Date: "01/20/26",
             Type: "Expense",
-            Account: "BofA 9155",
+            Account: "9155",
             Amount: 170000.98,
             Debit: null,
             Credit: 170000.98,
@@ -24,7 +24,7 @@ const Transactions = () => {
             id: 2,
             Date: "01/20/26",
             Type: "Income",
-            Account: "BofA 9155",
+            Account: "9155",
             Amount: 39999900.00,
             Debit: 63000.00,
             Credit: null,
@@ -75,7 +75,10 @@ const Transactions = () => {
 
                                     <td>{transaction.Date}</td>
                                     <td>{transaction.Type}</td>
-                                    <td>{transaction.Account}</td>
+                                    <td className='account'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#938D8D"><path d="M880-720v480q0 33-23.5 56.5T800-160H160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720Zm-720 80h640v-80H160v80Zm0 160v240h640v-240H160Zm0 240v-480 480Z"/></svg>
+                                        {transaction.Account}
+                                    </td>
 
                                     <td>
                                         <span className='sub mr-5'>$</span>
